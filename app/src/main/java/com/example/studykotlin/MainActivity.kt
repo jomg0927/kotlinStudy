447076569a2bc7b59f5f46935038b31841afe36c
Parkduksung <duksung1234@naver.com>
2021-03-06 17:12:23 +0900

    [CodeReview]Listener Part

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -1,63 +1,78 @@
-package com.example.myapplication
+package com.example.studykotlin
 
 import android.os.Bundle
-import android.os.Parcel
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.studykotlin.R
 
-class MainActivity() : AppCompatActivity(){
-    constructor(parcel: Parcel) : this() {
+class MainActivity : AppCompatActivity() {
+
+    var text: TextView? = null
+
+    var button5: Button? = null
+
+    private var result: Int? = 0
+
+
+    private val operatorListener = View.OnClickListener {
+        when (it.id) {
+            R.id.button5 -> {
+                text?.text = result.toString()
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text : TextView = findViewById(R.id.textView)
-        val edit1 : EditText = findViewById(R.id.value1)
-        val edit2 : EditText = findViewById(R.id.value2)
+        text = findViewById(R.id.textView)
 
-        val button1 : Button = findViewById(R.id.button)
-        val button2 : Button = findViewById(R.id.button2)
-        val button3 : Button = findViewById(R.id.button3)
-        val button4 : Button = findViewById(R.id.button4)
-        val button5 : Button = findViewById(R.id.button5)
+        button5 = findViewById(R.id.button5)
 
-        var result = 0
+        val edit1: EditText = findViewById(R.id.value1)
+        val edit2: EditText = findViewById(R.id.value2)
 
-        button2.setOnClickListener{
-            result = sum(edit1.text.toString().toInt(),edit2.text.toString().toInt())
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
+
+
+        button2.setOnClickListener {
+            result = sum(edit1.text.toString().toInt(), edit2.text.toString().toInt())
         }
 
-        button4.setOnClickListener{
-            result = div(edit1.text.toString().toInt(),edit2.text.toString().toInt())
+        button4.setOnClickListener {
+            result = div(edit1.text.toString().toInt(), edit2.text.toString().toInt())
         }
 
-        button3.setOnClickListener{
-            result = mul(edit1.text.toString().toInt(),edit2.text.toString().toInt())
+        button3.setOnClickListener {
+            result = mul(edit1.text.toString().toInt(), edit2.text.toString().toInt())
         }
 
-        button4.setOnClickListener{
-            result = sub(edit1.text.toString().toInt(),edit2.text.toString().toInt())
+        button4.setOnClickListener {
+            result = sub(edit1.text.toString().toInt(), edit2.text.toString().toInt())
         }
-        button5.setOnClickListener{
-            text.setText(result)
-        }
+
+        button5?.setOnClickListener(operatorListener)
 
     }
-    fun sum(num1:Int, num2:Int): Int{
+
+    fun sum(num1: Int, num2: Int): Int {
         return num1 + num2
     }
-    fun mul(num1:Int, num2:Int): Int{
+
+    fun mul(num1: Int, num2: Int): Int {
         return num1 * num2
     }
-    fun div(num1:Int, num2:Int): Int{
+
+    fun div(num1: Int, num2: Int): Int {
         return num1 / num2
     }
-    fun sub(num1:Int, num2:Int): Int{
+
+    fun sub(num1: Int, num2: Int): Int {
         return num1 - num2
     }
 }
