a54680d10386b8669f36ef31c588b8d5859c4974
Parkduksung <duksung1234@naver.com>
2021-03-06 18:04:00 +0900

    [CodeReview-DataBinding] dataBinding 적용.

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.studykotlin
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.studykotlin.databinding.ActivityMainBinding
@@ -13,47 +10,28 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    var text: TextView? = null
-
-    var plusButton: Button? = null
-    var minButton: Button? = null
-    var mulButton: Button? = null
-    var divButton: Button? = null
-    var yieldButton: Button? = null
-
-    var inputNum1: EditText? = null
-    var inputNum2: EditText? = null
-
     private var result: Int? = 0
 
     private val operatorListener = View.OnClickListener {
         when (it.id) {
             R.id.plus_button -> {
-                if (inputNum1 != null && inputNum2 != null) {
                     result =
-                        sum(inputNum1?.text.toString().toInt(), inputNum2?.text.toString().toInt())
-                }
+                        sum(binding.input1.text.toString().toInt(), binding.input2.text.toString().toInt())
             }
             R.id.min_button -> {
-                if (inputNum1 != null && inputNum2 != null) {
                     result =
-                        sub(inputNum1?.text.toString().toInt(), inputNum2?.text.toString().toInt())
-                }
+                        sub(binding.input1.text.toString().toInt(), binding.input2.text.toString().toInt())
             }
             R.id.mul_button -> {
-                if (inputNum1 != null && inputNum2 != null) {
                     result =
-                        mul(inputNum1?.text.toString().toInt(), inputNum2?.text.toString().toInt())
-                }
+                        mul(binding.input1.text.toString().toInt(), binding.input2.text.toString().toInt())
             }
             R.id.div_button -> {
-                if (inputNum1 != null && inputNum2 != null) {
                     result =
-                        div(inputNum1?.text.toString().toInt(), inputNum2?.text.toString().toInt())
-                }
+                        div(binding.input1.text.toString().toInt(), binding.input2.text.toString().toInt())
             }
             R.id.yield_button -> {
-                text?.text = result.toString()
+                binding.result.text = result.toString()
             }
         }
     }
@@ -63,22 +41,11 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        text = findViewById(R.id.result)
-
-        inputNum1 = findViewById(R.id.input1)
-        inputNum2 = findViewById(R.id.input2)
-
-        plusButton = findViewById(R.id.plus_button)
-        minButton = findViewById(R.id.min_button)
-        mulButton = findViewById(R.id.mul_button)
-        divButton = findViewById(R.id.div_button)
-        yieldButton = findViewById(R.id.yield_button)
-
-        plusButton?.setOnClickListener(operatorListener)
-        minButton?.setOnClickListener(operatorListener)
-        mulButton?.setOnClickListener(operatorListener)
-        divButton?.setOnClickListener(operatorListener)
-        yieldButton?.setOnClickListener(operatorListener)
+        binding.plusButton.setOnClickListener(operatorListener)
+        binding.minButton.setOnClickListener(operatorListener)
+        binding.mulButton.setOnClickListener(operatorListener)
+        binding.divButton.setOnClickListener(operatorListener)
+        binding.yieldButton.setOnClickListener(operatorListener)
     }
 
     fun sum(num1: Int, num2: Int): Int {
